9604ce97399151adef60caa53ab9fe42f3933bdd
Giuliano Accorsi <giulianoaccorsi@gmail.com>
2021-03-03 03:12:16 -0300

    Inicia Projeto Movie

---
+++ TheCleanMovie/TheCleanMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheCleanMovie
+//
+//  Created by Giuliano Accorsi on 03/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
